e56819bf2225f7b70b98e90cf8e003e7ca268cca
Petrik Lajos Szg <info@petrik.hu>
2018-09-21 15:30:32 +0200

    Az elso feladat elkeszitve, kivalasztva az elso 10 kozul a legnagyobbat

--- Sajtospufi/Program.cs
+++ Sajtospufi/Program.cs
@@ -9,7 +9,24 @@ namespace Sajtospufi
     class Program
     {
         static void Main(string[] args)
-        { 
+        {
+            Console.WriteLine("Kérem adjon meg 10 számot!");
+            int i = 0;
+            int legnagyobb = 0;
+            int[] szamok = new int[10];
+            do
+            {
+                Console.Write("\nA(z) {0}. szám: ", i + 1);
+                szamok[i] = Convert.ToInt32(Console.ReadLine());
+                if (szamok[i] > legnagyobb)
+                {
+                    legnagyobb = szamok[i];
+                }
+                i++;
+            } while (i != szamok.Length);
+            Console.WriteLine("A legnagyobb megadott szám: {0}",legnagyobb);
+
+            Console.ReadKey();
         }
     }
 }
